66cb55998e3423b91f45e81ca2486c8ca39e1f94
jtalverson <jtal228@uky.edu>
2023-02-15 20:06:08 -0500

    Tutorial updates

---
+++ Assets/Scripts/MoveRX.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRX : MonoBehaviour
+{
+    public float moveDistance = .5f;
+    public float degrees = 1f;
+
+    public Transform circle;
+
+/*    private void Start()
+    {
+        circle = GameObject.Find("Circle").GetComponent<Transform>();
+    }*/
+
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(moveDistance, 0f, 0f);
+        circle.position += new Vector3(moveDistance, 0f, 0f);
+        transform.eulerAngles += Vector3.forward * degrees;
+    }
+}
