c2bd1d12bd85fe7f5aa959147b6121d7ae240699
Duong Hoang <duong.hoang@uky.edu>
2023-04-06 13:50:17 -0400

    add reading property configs as integers

--- Assets/Scripts/PopulateDiff.cs
+++ Assets/Scripts/PopulateDiff.cs
@@ -2,23 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using TMPro;
 
 public class PopulateDiff : MonoBehaviour
 {
-    private int columnCount = 5;
-    public TextAsset textAssetData;
+    private int propertyCount = 5;
+    public TextAsset textAssetData; 
 
+    /// Class <c>Difficulty</c> models a difficulty level with numeric constraints.
     [System.Serializable]
     public class Difficulty
-    {
-        public string conversionCount;
-        public string timePerConversion;
-        public string calculationCount;
-        public string timePerCalculation;
-        public string strikeLimit;
+    {   
+        // Properties
+        public int ConversionCount; // Number of conversion problems in a set
+        public int TimePerConversion; // Time (in secs) allowed to spend on a conversion problem
+        public int CalculationCount; // Number of calculation problems in a set
+        public int TimePerCalculation; // Time (in secs) allowed to spend on a calculation problem
+        public int StrikeLimit; // Number of incorrect answers allowed before game forced to be over
     }
 
+    /// Class <c>DifficultyList</c> holds a list of all the difficulty levels' configurations.
     [System.Serializable]
     public class DifficultyList
     {
@@ -30,22 +32,21 @@ public class PopulateDiff : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Read configurations from csv file
         string[] data = textAssetData.text.Split(new string[] { ",", "\n" }, StringSplitOptions.None);
-        int tableSize = data.Length / columnCount - 1;
-        // for (int i = 0; i < data.Length; i++)
-        //     Debug.Log(data[i]);
-        //Debug.Log(data[5*1]);
+        int tableSize = data.Length / propertyCount - 1; // Number of properties
+        
         config.populateData = new Difficulty[tableSize];
     
         for (int i = 0; i < tableSize; i++)
         {
-            Debug.Log(columnCount * (i+1) + 1);
-            config.populateData[i] = new Difficulty();
-            config.populateData[i].conversionCount = data[columnCount * (i+1)];
-            config.populateData[i].timePerConversion = data[columnCount * (i+1) + 1];
-            config.populateData[i].conversionCount = data[columnCount * (i+1) + 2];
-            config.populateData[i].timePerConversion = data[columnCount * (i+1) + 3];
-            config.populateData[i].strikeLimit = data[columnCount * (i+1) + 4];
+            // Assign each level's configuration to corresponding property
+            config.populateData[i] = new Difficulty(); 
+            config.populateData[i].ConversionCount = Int32.Parse(data[propertyCount * (i+1)]);
+            config.populateData[i].TimePerConversion = Int32.Parse(data[propertyCount * (i+1) + 1]);
+            config.populateData[i].CalculationCount = Int32.Parse(data[propertyCount * (i+1) + 2]);
+            config.populateData[i].TimePerCalculation = Int32.Parse(data[propertyCount * (i+1) + 3]);
+            config.populateData[i].StrikeLimit = Int32.Parse(data[propertyCount * (i+1) + 4]);
         }
     }
 }
