0df4b310c9c6e440001d3fedeec6a4621d2af603
cpemberton3 <chas@webpemberton.com>
2023-04-10 12:43:17 -0400

    scoring system

--- Assets/Scripts/ScoringSystem.cs
+++ Assets/Scripts/ScoringSystem.cs
@@ -5,6 +5,7 @@ public class ScoringSystem : MonoBehaviour
     public float score;
     public float multiplier;
     static private float multiplierIncrease = 0.2f;
+    static private float baseScore = 100000.0;
 
     public void Start()
     {
@@ -12,16 +13,21 @@ public class ScoringSystem : MonoBehaviour
         multiplier = 1.0f;
     }
 
-    // Update is called after every swipe
-    public void ScoreUpdate(float points)
+    /* Update is called after every swipe
+     * startTime - the time on the timer when the problem is first shown
+     * currentTime - the time left on the timer when the problem is answered
+     * correct - if the question was scored correctly or not
+    */
+    public void ScoreUpdate(float startTime, float currentTime, bool correct)
     {
-        if (points > 0)
+        float inverseTimeLeft = baseScore - (startTime - currentTime); // ASSUME startTime - currentTime > 0
+        if (correct == true) // add to score
         {
             multiplier += multiplierIncrease;
-            score += points;
+            score += inverseTimeLeft;
             score = score * multiplier;
         }
-        else
+        else // add no points, reset multiplier
         {
             multiplier = 1.0f;
         }
